46f047032d51c7771fe18c2b9ebbce9e6f9c418e
Kobus van Schoor <v.schoor.kobus@gmail.com>
2020-09-27 10:34:56 +0200

    initial commit

---
+++ partsware/parts/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from parts.models import Tag, Container, Part
+
+# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Container)
+admin.site.register(Part)
---
+++ partsware/parts/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+# represents a physical container within which parts will be stored.
+class Container(models.Model):
+    # user-friendly name for the container
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    # regex describing a valid naming scheme for locations in this container
+    naming_scheme = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+# represents a physical part
+class Part(models.Model):
+    # user with which the part is associated
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # user-friendly name of the part
+    name = models.CharField(max_length=50)
+    # slug generated from the name, should be unique within the user's parts
+    slug = models.SlugField(blank=True)
+
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    description = models.TextField(blank=True)
+    datasheet = models.FileField(blank=True)
+    pinout = models.ImageField(blank=True)
+
+    quantity = models.PositiveSmallIntegerField(default=0)
+    price = models.DecimalField(blank=True, max_digits=10, decimal_places=2)
+
+    container = models.ForeignKey(Container, on_delete=models.PROTECT)
+    # this location should be verified against the relevant container's naming
+    # scheme
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
