a4b19305712035da1d57964b1e870bdb504dad13
Kobus van Schoor <v.schoor.kobus@gmail.com>
2020-09-27 13:46:54 +0200

    added login page

--- partsware/partsware/settings.py
+++ partsware/partsware/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'partsware.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
