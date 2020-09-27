2aba67b38abf7bb6f5517324bf4f0b0a9650e9bb
Kobus van Schoor <v.schoor.kobus@gmail.com>
2020-09-27 20:27:28 +0200

    fix make run for multiple interfaces

--- partsware/parts/urls.py
+++ partsware/parts/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'parts'
 
 urlpatterns = [
-        path('index/', views.index, name='index')
-        ]
+    path('index/', views.index, name='index'),
+    path('search/', views.search, name='search'),
+]
--- partsware/parts/views.py
+++ partsware/parts/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+import re
+
+from functools import reduce
+
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.db.models import Q
 
 from .models import Tag, Container, Part
 
@@ -16,3 +22,38 @@ def index(request):
     }
 
     return render(request, 'parts/index.html', context=context)
+
+@login_required
+@require_POST
+def search(request):
+    # retrieve query
+    query = request.POST.get("search", None).strip()
+    if not query:
+        return redirect('parts:index')
+
+    # pre-process query to all-lowercase and remove special chars
+    query = query.lower()
+    query = re.sub("[^0-9a-zA-Z]+", " ", query)
+
+    # split query into its separate words
+    query = query.split()
+
+    def construct_query(word):
+        q = Q(name__contains=word)
+        q |= Q(description__contains=word)
+
+        return q
+
+    # build query from words
+    q = reduce(lambda x, y: x | y, [construct_query(word) for word in query])
+
+    # filter parts
+    parts = Part.objects.filter(user=request.user)
+    parts = parts.filter(q)
+
+    # return results
+    context = {
+        'parts': parts
+    }
+
+    return render(request, 'parts/search.html', context=context)
