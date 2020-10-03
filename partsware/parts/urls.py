781ac32c53386d2a3da6ec3747873fe574802c93
Kobus van Schoor <v.schoor.kobus@gmail.com>
2020-10-03 11:08:51 +0200

    added all parts url

--- partsware/parts/urls.py
+++ partsware/parts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('media/<str:media_type>/<int:part_id>/', views.media, name='media'),
     path('search/', views.search, name='search'),
+    path('all_parts/', views.all_parts, name='all_parts'),
     path('add_container/', views.add_container, name='add_container'),
     path('part/<int:part_id>/', views.view_part, name='view_part'),
     path('add_part/', views.add_part, name='add_part'),
